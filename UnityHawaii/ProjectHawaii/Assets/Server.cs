537793a0418212068c57c62c3df943b2b0f3e988
goblinnl <stefan.bijen@gmail.com>
2018-01-27 14:51:57 +0100

    Add server

---
+++ UnityHawaii/ProjectHawaii/Assets/Server.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Server : MonoBehaviour {
+
+    NetworkClient client;
+    bool isAtStartup = false;
+
+    public void SetupServer()
+    {
+        NetworkServer.Listen(4444);
+    }
+
+    public void SetupClient()
+    {
+        client = new NetworkClient();
+        client.RegisterHandler(MsgType.Connect, OnConnected);
+    }
+
+    public void OnConnected(NetworkMessage rMessage)
+    {
+        Debug.Log("Connected to Server");
+    }
+
+	// Use this for initialization
+	void Start () {
+        SetupServer();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
