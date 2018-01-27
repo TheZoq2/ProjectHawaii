3cd8782f5edbfb5dd79f2d98053976830da2e9b3
TheZoq2 <frans.skarman@gmail.com>
2018-01-27 15:56:54 +0100

    Add networking code

---
+++ UnityHawaii/ProjectHawaii/Assets/Scipts/Client.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Messages;
+
+public class Client : MonoBehaviour {
+    NetworkClient client;
+
+    bool isConnected = false;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void SetupClient()
+    {
+        client = new NetworkClient();
+        client.RegisterHandler(MsgType.Connect, OnConnected);
+        client.Connect("localhost", 4444);
+    }
+
+    public void OnConnected(NetworkMessage rMessage)
+    {
+        Debug.Log("Connected to Server");
+
+        var message = new TestMessage();
+        message.message = "Your mom";
+
+        client.Send(888, message);
+
+        isConnected = true;
+    }
+
+    void OnMouseDown() {
+        Debug.Log("Starting client");
+
+        if(!isConnected) {
+            SetupClient();
+        }
+    }
+}
--- UnityHawaii/ProjectHawaii/Assets/Scipts/Server.cs
+++ UnityHawaii/ProjectHawaii/Assets/Scipts/Server.cs
@@ -2,36 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine;
+using Messages;
 
 public class Server : MonoBehaviour {
 
-    NetworkClient client;
     bool isAtStartup = false;
 
     public void SetupServer()
     {
+        NetworkServer.RegisterHandler(888, On888);
         NetworkServer.Listen(4444);
     }
 
-    public void SetupClient()
-    {
-        client = new NetworkClient();
-        client.RegisterHandler(MsgType.Connect, OnConnected);
+
+    // Use this for initialization
+    void Start () {
+        //SetupServer();
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
     }
 
-    public void OnConnected(NetworkMessage rMessage)
-    {
-        Debug.Log("Connected to Server");
-    }
-
-	// Use this for initialization
-	void Start () {
+    void OnMouseDown() {
+        Debug.Log("Server starting");
         SetupServer();
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void On888(NetworkMessage msg) {
+        Debug.Log("got test message");
+    }
 }
