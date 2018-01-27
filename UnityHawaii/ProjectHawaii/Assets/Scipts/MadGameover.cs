2bcc1d03439d67fe252c2e62b7835da3073b7f25
Ilya Efimov <madcoder39@gmail.com>
2018-01-28 00:16:58 +0100

    - pushed shit bam

---
+++ UnityHawaii/ProjectHawaii/Assets/Scipts/MadGameover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MadGameover : MonoBehaviour {
+
+    [SerializeField] Text timeText;
+
+	// Use this for initialization
+	void Start () {
+        timeText.text += PlayerPrefs.GetString("playTime", "None");
+    }
+	
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+}
