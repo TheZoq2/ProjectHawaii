4e6885b365b567aeb393132084540a571e2291dd
Slavi Stoev <sl_stoev@abv.bg>
2018-01-27 11:02:00 +0100

    Some stuff I was working on Phaser previously...

---
+++ GlobalGamejamPrototype/GlobalGamejamPrototype/controls/button.ts
@@ -0,0 +1,39 @@
+﻿class Button {
+    public name: string;
+    //game: Phaser.Game;
+    private buttonSprite: Phaser.Button;
+    private graphics: Phaser.Graphics;
+
+    public constructor(private game: Phaser.Game, public symbol: string, public x: number, public y: number) {
+        this.name = "Button " + this.symbol + " sprite";
+
+        this.preload();
+        this.create();
+    }
+
+    private preload(): void {
+        //this.game.load.
+    }
+
+    private create(): void {
+        this.graphics = this.game.add.graphics(300, 300);
+        this.graphics.lineStyle(0);
+        this.graphics.beginFill(0xff0000);
+        this.graphics.drawEllipse(this.x, this.y, 100, 50);
+        this.graphics.endFill();
+
+        //this.buttonSprite = this.game.add.button
+        //    (this.x, this.y, this.name, this.onSymbolButtonClick);
+
+        //this.buttonSprite.anchor.setTo(0.5, 0.5);
+
+    }
+    private update(): void {
+
+    }
+
+    private onSymbolButtonClick(): void {
+        alert("Hello!");
+    }
+
+}
