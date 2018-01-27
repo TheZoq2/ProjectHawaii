5ffdb8782b77597945954e7004b39b5ac7f59678
Ilya Efimov <madcoder39@gmail.com>
2018-01-27 12:39:44 +0100

    - dotween + level logic

---
+++ UnityHawaii/ProjectHawaii/Assets/Scipts/MadSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MadSlider : MonoBehaviour {
+
+    [SerializeField] RectTransform[] points;
+    [SerializeField] RectTransform mover;
+
+
+    [SerializeField, ReadOnly] int currentId;
+
+    public void SetIsland(int id)
+    {
+        mover.position = points[id].position;
+        currentId = id;
+    }
+}
