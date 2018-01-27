40c0dec945591bdec90e343807605efb8779e6b8
DESKTOP-0LUJTLU\Ben <bengunnewijk@hotmail.com>
2018-01-27 15:57:29 +0100

    The scene and assets for the steering wheel

---
+++ UnityHawaii/ProjectHawaii/Assets/SteeringWheelStuffBen/GUIWheel.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class GUIWheel : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler{
+
+    private float wheelAngle;
+    private float rollBackTimer = 0;
+    private float rollBackTimerStartValue = 3f;
+
+    public float wheelRotationGoal = 90;
+    public float GoalPrecision = 10;
+    public float CurrentRotation = 0;
+    private bool isRotating = false;
+
+    public Text text;
+    public LedScript led;
+    public UnityEvent onValueChange;
+
+    // Use this for initialization
+    void Start () {
+        text.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rollBackTimer > 0)
+        {
+            rollBackTimer -= Time.deltaTime;
+            //RollBack();
+        }
+        if (isRotating)
+        {
+            if (transform.localRotation.eulerAngles.z  > wheelRotationGoal - GoalPrecision && 
+                transform.localRotation.eulerAngles.z  < wheelRotationGoal + GoalPrecision)
+            {
+                led.LightUp();
+            }
+            else
+            {
+                led.LightOff();
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //CancelRollBack();
+
+        var dir = Input.mousePosition - transform.position;
+        wheelAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        wheelAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+        
+        isRotating = true;
+        
+    }
+    
+    public void OnDrag(PointerEventData eventdata)
+    {
+        float oldRotation = transform.rotation.eulerAngles.z;
+
+        Vector3 dir = Input.mousePosition - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - wheelAngle ;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        CurrentRotation = transform.localRotation.eulerAngles.z;
+        text.text = CurrentRotation.ToString();
+        onValueChange.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventdata)
+    {
+        isRotating = false;
+        //StartRollBack();
+    }
+    /*
+    private void StartRollBack()
+    {
+        rollBackTimer = rollBackTimerStartValue;
+    }
+    
+    private void RollBack()
+    {
+        wheelAngle = Mathf.Lerp(-90, wheelAngle, rollBackTimer / rollBackTimerStartValue);
+        transform.rotation = Quaternion.AngleAxis(wheelAngle, Vector3.forward);
+    }
+
+    private void CancelRollBack()
+    {
+        rollBackTimer = 0;
+    }*/
+}
---
+++ UnityHawaii/ProjectHawaii/Assets/SteeringWheelStuffBen/LedScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LedScript : MonoBehaviour {
+
+    public Sprite LedOff;
+    public Sprite LedOn;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Image>().sprite = LedOff;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LightUp()
+    {
+        GetComponent<Image>().sprite = LedOn;
+    }
+
+    public void LightOff()
+    {
+        GetComponent<Image>().sprite = LedOff;
+    }
+}
